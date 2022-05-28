84089591e6689c4ccf1e0a47b8094181856d5820
Sergey <wer113wer@yandex.ru>
2022-05-28 12:33:38 +0300

    Modified receipt of input data, initial verification of input files

--- header.h
+++ header.h
@@ -4,17 +4,35 @@
 #endif // HEADER_H
 #include <QString>
 #include <QList>
+#include <QFile>
 
+// Структура сотрудника
 struct employee
 {
    QString fioEmployee; // ФИО сотрудника
    int idEmployee;  // ID сотрудника
 };
 
+// Структура подразделения
 struct department
 {
     int idHead; // ID директора подразделения
     QList<employee> employeesOfDepartment;  // Массив сотрудников подразделения
 };
 
+/*! Проверка входных данных (.xml и .txt файлов)
+*   \return true, false
+*/
+bool testInputDatas();
 
+/*! Ошибки входных данных
+*   \param[in] error        // ошибка
+*   \param[out] int         // номер ошибки
+*/
+int outputError(int error);
+
+/*! Получение входных данных
+ *
+ *
+ */
+ void getInputDatas();
--- main.cpp
+++ main.cpp
@@ -4,41 +4,97 @@
 #include <QFile>
 #include <QString>
 #include <QDomDocument>
+#include <QDir>
 #include "header.h"
 
+// Объявление входного .xml файла
+QString locationXml = QDir::currentPath();  // Расположение входного .xml файла             // сунуть в глобальные переменные  inputXml и inputTxt
+QFile inputXml(locationXml + "..\\..\\kinpohierarchy\\input.xml");                          // использовать алгоритм Дейкстры:? глубь или ширина
+
+// Объявление входного .txt файла
+QString locationTxt = QDir::currentPath();  // Расположение входного .txt файла             // сунуть в отдельную функцию input
+QFile inputTxt(locationTxt + "..\\..\\kinpohierarchy\\input2.txt");
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    // Объявление входного .xml файла
-    QFile inputXml("input.xml");
+    getInputDatas();
 
-    // Проверка наличия входного файла .xml
-    if (!inputXml.exists())
-    {
-        puts("error: no input .xml");
+    // Выполнение проверки входных данных
+    try {
+        testInputDatas();
+
+
+    }  catch (int error) {
+        qDebug() << outputError(error);
+        inputXml.close();
+        inputTxt.close();
         return 0;
+
     }
 
-    // Проверка корректности входного .xml файла
-    if (!inputXml.open(QIODevice::ReadOnly | QIODevice::Text))
+
     {
-        puts("invalid input data: unable to read .xml file");
-        return 0;
+        inputXml.close();
+        inputTxt.close();
+        qDebug() << "work";
     }
 
-
-
-    // Объявление входного .txt файла
-    QFile inputTxt("input2.txt");
-
-    // Проверка наличия входного файла .txt
-    if (!inputTxt.exists())
-    {
-        puts("error: no input .txt");
-        return 0;
-    }
 }
 
 
+bool testInputDatas()           //через try catch
+{
+        // Проверка наличия входного файла .xml
+        if (!inputXml.exists())
+        {
+            //puts("error: no input .xml");
+            throw 1;
+        }
+
+        // Проверка корректности входного .xml файла
+        if (!inputXml.open(QIODevice::ReadOnly | QIODevice::Text))
+        {
+            //puts("invalid input data: unable to read .xml file");
+            throw 2;
+        }
+
+        // Проверка наличия входного файла .txt
+        if (!inputTxt.exists())
+        {
+            //puts("error: no input .txt");
+            throw 3;
+        }
+
+        // Проверка наличия ID во входном .txt файле
+        if(inputTxt.size() == 0)
+        {
+            //puts("invalid input data: .txt file is empty");
+            throw 4;
+        }
+}
+
+int outputError(int error)
+{
+    if(error == 1)
+        qDebug() << "error: no input .xml";
+    if(error == 2)
+        qDebug() << "invalid input data: unable to read .xml file";
+    if(error == 3)
+        qDebug() << "error: no input .txt";
+    if(error == 4)
+        qDebug() << "invalid input data: .txt file is empty";
+}
+
+void getInputDatas()
+{
+    // Объявление входного .xml файла
+    QString locationXml = QDir::currentPath();  // Расположение входного .xml файла             // сунуть в глобальные переменные  inputXml и inputTxt
+    QFile inputXml(locationXml + "..\\..\\kinpohierarchy\\input.xml");                          // использовать алгоритм Дейкстры:? глубь или ширина
+
+    // Объявление входного .txt файла
+    QString locationTxt = QDir::currentPath();  // Расположение входного .txt файла             // сунуть в отдельную функцию input
+    QFile inputTxt(locationTxt + "..\\..\\kinpohierarchy\\input2.txt");
+
+}
